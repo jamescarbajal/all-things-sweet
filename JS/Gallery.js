223979e2e5796a4ea04ac10f91ec4b0685e43d04
James C <you@example.com>
2022-05-28 12:59:10 -0700

    feat: add image filter javascript

---
+++ JS/Gallery.js
@@ -0,0 +1,45 @@
+// Filter Items
+const filterContainer = document.querySelector(".gallery-filter");
+const galleryItems = document.querySelectorAll(".gallery-item");
+const galleryCont = document.querySelectorAll("gallery-container");
+
+filterContainer.addEventListener("click", (event) =>{
+    if(event.target.classList.contains("filter-item")){
+ 
+      // deactivate existing active 'filter-item'
+        filterContainer.querySelector(".active").classList.remove("active");
+ 
+      // activate new 'filter-item'
+        event.target.classList.add("active");
+ 
+        const filterValue = event.target.getAttribute("data-filter");
+ 
+        galleryItems.forEach((item) =>{
+ 
+        if(item.classList.contains(filterValue) || filterValue === 'all'){
+            item.classList.remove("hide");
+             item.classList.add("show");
+        }
+ 
+        else{
+            item.classList.remove("show");
+            item.classList.add("hide");
+        }
+ 
+         });
+        
+        galleryCont.forEach((cont) =>{
+        
+        if(cont.childNodes.length === 0) {
+            cont.classList.remove("show");
+            cont.classList.add("hide");
+        }
+        
+        else{
+            cont.classList.remove("hide");
+            cont.classList.add("show");
+        }
+
+         });
+    }
+  });
